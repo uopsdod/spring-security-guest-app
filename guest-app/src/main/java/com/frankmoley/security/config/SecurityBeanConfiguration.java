9edc0415bce69fb3404f67af8351a87d2c29a7c2
uopsdod <uopsdod@gmail.com>
2018-08-22 04:02:35 +0800

    1. change your password to bcrypted hash 2. change DaoAuthenticationProvider instance Encoder config

--- guest-app/src/main/java/com/frankmoley/security/config/SecurityBeanConfiguration.java
+++ guest-app/src/main/java/com/frankmoley/security/config/SecurityBeanConfiguration.java
@@ -3,6 +3,7 @@ package com.frankmoley.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import com.frankmoley.security.app.auth.MyUserDetailsService;
 
@@ -13,7 +14,8 @@ public class SecurityBeanConfiguration {
 	public DaoAuthenticationProvider authenticationProvider(MyUserDetailsService myUserDetailsService) {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(myUserDetailsService);
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+//		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
 		return provider;
 	}
 	
