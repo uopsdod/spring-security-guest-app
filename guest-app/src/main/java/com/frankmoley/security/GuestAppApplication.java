cee1fc13ce535678fc4485010fdf6db6ac77818b
uopsdod <uopsdod@gmail.com>
2018-08-21 23:54:53 +0800

    organize the structure

--- guest-app/src/main/java/com/frankmoley/security/GuestAppApplication.java
+++ guest-app/src/main/java/com/frankmoley/security/GuestAppApplication.java
@@ -1,8 +1,11 @@
-package com.frankmoley.security.app;
+package com.frankmoley.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
 @SpringBootApplication
 public class GuestAppApplication {
 
--- guest-app/src/main/java/com/frankmoley/security/config/MyWebSecurityConfigurerAdapter.java
+++ guest-app/src/main/java/com/frankmoley/security/config/MyWebSecurityConfigurerAdapter.java
@@ -1,10 +1,12 @@
-package com.frankmoley.security.app;
+package com.frankmoley.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter{
 
 	@Override
